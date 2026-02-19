7ffc3c9ffe199bbf55ca68be1bf1a34834d8e80b
TaKakun33 <akmalkaflianan33@gmail.com>
2026-02-19 17:28:04 +0700

    tambahkan komentar di main

--- Pertemuan1/src/MTitik.java
+++ Pertemuan1/src/MTitik.java
@@ -6,11 +6,11 @@
 
 public class MTitik{
     public static void main(String[] args) {
-        Titik T1 = new Titik();
-        T1.setAbsis(3);
-        T1.setOrdinat(4);
-        T1.printTitik(); 
-        T1.geser(3,4);
-        T1.printTitik(); 
+        Titik T1 = new Titik();  // Membuat objek Titik T1 (0,0)
+        T1.setAbsis(3);       // Menghubah absis Titik T1 dengan nilai 3
+        T1.setOrdinat(4);     // Menghubah ordinat Titik T1 dengan nilai 4
+        T1.printTitik();        // Mencetaak Koordinat Titik ke layar
+        T1.geser(3,4);     // Menggeser T1 sejauh (3,4)
+        T1.printTitik();        // Mencetaak Koordinat Titik ke layar
     }
 }
