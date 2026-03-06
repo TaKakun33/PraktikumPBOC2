ac904c6a52e8a8ae5f51758d4176d7234905b3d3
TaKakun33 <akmalkaflianan33@gmail.com>
2026-03-06 07:57:06 +0700

    menyelesaikan praktikum 3

--- Pertemuan3/src/MMahasiswa.java
+++ Pertemuan3/src/MMahasiswa.java
@@ -1,5 +1,5 @@
 public class MMahasiswa {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
 
         /* Mengecek class Dosen */
         Dosen A = new Dosen();
