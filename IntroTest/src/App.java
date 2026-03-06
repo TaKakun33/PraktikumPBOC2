e4544e6f59f8d35010beabcd201ae27b61232178
TaKakun33 <akmalkaflianan33@gmail.com>
2026-03-06 07:54:14 +0700

    menambahkan gitmore dan hapus file .class

--- IntroTest/src/App.java
+++ IntroTest/src/App.java
@@ -1,4 +1,4 @@
-public class App {
+public class App { 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("\n Contoh 1: Variables dan Conditionals ");
