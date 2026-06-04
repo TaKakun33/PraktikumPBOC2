3cb1844f361739061f5e81d6389e2d18a1f8f9a0
TaKakun33 <akmalkaflianan33@gmail.com>
2026-06-04 18:22:26 +0700

    Selesikan pertemuan 12

---
+++ Pertemuan12/src/LambdaList.java
@@ -0,0 +1,19 @@
+/**
+ * File: LambdaList.java
+ * Deskripsi: Implementasi lambda pada List, digunakan sebagai parameter
+ * pada method.
+ */
+
+import java.util.ArrayList;
+
+public class LambdaList {
+    public static void main(String[] args) {
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Didi");
+
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
+}
